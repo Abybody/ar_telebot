3416967244098ca7b4c586de67728d606104b0a3
Abybody <96799010+Abybody@users.noreply.github.com>
2022-04-03 22:11:52 +0300

    add files
    adds

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = '5113745614:AAGUbDuyqo8D1T2-JXeDCwmlmYjW-cNvwP8'
+
+CHANNEL_ID_ADM = '-1001753057517' # канал куда быстрее всего приходят логи и ниже тоже
+CHANNEL_ID_NEED_ADM = '-1001599896765'
